c4d6f84bc05bc96ec3108c4674966ef9553c5390
unknown <you@example.com>
2024-10-14 15:36:14 +0700

    first commit

---
+++ EF_DBFirst/Data/DatabaseFirstContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using EF_DBFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_DBFirst.Data;
+
+public partial class DatabaseFirstContext : DbContext
+{
+    public DatabaseFirstContext()
+    {
+    }
+
+    public DatabaseFirstContext(DbContextOptions<DatabaseFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<DbFirst> DbFirsts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-7PPNR21\\MSSQLSERVER01;Initial Catalog=database_first;Integrated Security=True;Pooling=False;Encrypt=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbFirst>(entity =>
+        {
+            entity.Property(e => e.Lop).IsFixedLength();
+            entity.Property(e => e.Ten).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EF_DBFirst/Models/DbFirst.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_DBFirst.Models;
+
+[Keyless]
+[Table("db_first")]
+public partial class DbFirst
+{
+    [StringLength(10)]
+    public string? Ten { get; set; }
+
+    public int? Tuoi { get; set; }
+
+    [StringLength(10)]
+    public string? Lop { get; set; }
+}
